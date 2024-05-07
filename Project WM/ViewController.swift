c12a0f619e2d08cb6a8a624826f5ea5b3e71bc41
 User <user@MacBook-Pro.local>
2024-05-07 21:34:21 +0300

    set up project

---
+++ Project WM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project WM
+//
+//  Created by  User on 07.05.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Project WM/WorkManagementDaTabase.swift
@@ -0,0 +1,14 @@
+//
+//  WorkManagementDaTabase.swift
+//  Project WM
+//
+//  Created by  vovanso on 07.05.2024.
+//
+
+import Foundation
+import UIKit
+import os.log
+
+class WorkManagementDaTabase {
+    
+}
