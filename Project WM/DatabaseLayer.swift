b874eb93af531051c2470f286f4c55ccede36d97
vanso <126582742+VOVANSO2207@users.noreply.github.com>
2024-05-09 08:12:46 +0300

    Created UI login

--- Project WM/DatabaseLayer.swift
+++ Project WM/DatabaseLayer.swift
@@ -9,6 +9,6 @@ import Foundation
 import UIKit
 import os.log
 
-class WorkManagementDaTabase {
+class DatabaseLayer {
     
 }
---
+++ Project WM/LoginViewController.swift
@@ -0,0 +1,37 @@
+//
+//  LoginViewController.swift
+//  Project WM
+//
+//  Created by  User on 09.05.2024.
+//
+
+import UIKit
+class LoginViewController : UIViewController {
+    @IBOutlet weak var passwordTextField: UITextField!
+  
+    @IBOutlet weak var usernameTextField: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Thiet lap cac thuoc tinh cho textField
+        usernameTextField.layer.borderColor = UIColor.purple.cgColor
+        usernameTextField.layer.borderWidth = 3
+        usernameTextField.layer.cornerRadius = 15.0
+        passwordTextField.layer.borderColor = UIColor.purple.cgColor
+        passwordTextField.layer.borderWidth = 3
+        passwordTextField.layer.cornerRadius = 15.0
+        
+        
+    }
+  
+    @IBAction func signInButton(_ sender: UIButton) {
+        // kiem tra tai khoan va mat khau
+//        guard let username = usernameTextField.text! , !username.isEmpty else{
+//
+//        }
+        print("Called")
+    }
+    func showAlert (withTitle title:String,message:String)
+    {
+        
+    }
+}
